008a0aecb7977e9b45eab7f91faa8adbaa8faff5
sou763 <>
2022-06-01 17:06:31 +0900

    fix:importを修正しました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
+import ReturnTopButton from '@/components/Button/Returntop';
 import Hero from '@/components/heroSections/Hero';
 import Information from '@/components/InformationSection/Information';
 import Layout from '@/components/Layouts/Layout';
 import News from '@/components/newsSection/News';
 import Service from '@/components/serviceSection/Service';
-// eslint-disable-next-line import/order
-import ReturnTopButton from '@/components/Button/Returntop';
 
 const Home = (): JSX.Element => {
   return (
